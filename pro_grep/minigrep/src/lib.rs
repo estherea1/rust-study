f5d099a730d4dbbd1118b4b0f0ccf5f1379df04f
esthereal <estherea1@users.noreply.github.com>
2024-06-23 05:09:58 +0800

    Add search function

--- pro_grep/minigrep/src/lib.rs
+++ pro_grep/minigrep/src/lib.rs
@@ -22,6 +22,37 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     //fs.read_to_string(file_path) 返回包含其内容的 std::io::Result<String>
     let contents = fs::read_to_string(config.file_path)?;
-    println!("内容：{contents}");
+    
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
+
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents:&'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
